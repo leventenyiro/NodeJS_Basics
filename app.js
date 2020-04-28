93e8dbecb6571d3b97c97a55e912fbecb897ac2e
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 11:09:56 +0200

    EventEmitter

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const tutorial = require("./tutorial")
+const EventEmitter = require("events")
+const eventEmitter = new EventEmitter()
 
-console.log(tutorial.sum(1,2))
-console.log(tutorial.PI)
-console.log(new tutorial.SomeMathObject())
+eventEmitter.on("tutorial", (num1, num2) => {
+    console.log(num1 + num2)
+})
+
+eventEmitter.emit("tutorial", 1, 2)
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super()
+        this._name = name
+    }
+
+    get name() {
+        return this._name
+    }
+}
+
+let ember = new Person("Ember")
+let valaki = new Person("Valaki")
+valaki.on("name", () => {
+    console.log("my name is " + valaki.name)
+})
+ember.on("name", () => {
+    console.log("my name is " + ember.name)
+})
+
+ember.emit("name")
+valaki.emit("name")
