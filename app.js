937617afd0e844c9716f931351337dcddd8346ed
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 23:00:00 +0200

    Express JOI 1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express")
 var path = require("path")
+var Joi = require("joi")
 var bodyParser = require("body-parser")
 var app = express()
 
 app.use("/public", express.static(path.join(__dirname, "static")))
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "static", "index.html"))
@@ -13,9 +13,19 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     console.log(req.body)
-    // DB here
-    //res.send("Successfully posted data")
-    res.json({success : true})
+    var schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    })
+    Joi.validate(req.body, schema, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send("An error has occured")
+        } else {
+            console.log(result)
+            res.send("Successfully posted data")
+        }
+    })
 })
 
 app.get("/example", (req, res) => {
