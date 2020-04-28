ebcf33da58d8267aae1ea9880b0daa42f44c99fd
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 16:57:43 +0200

    Express 1

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// npm install lodash
+var express = require("express")
+var app = express()
 
-// ha egy újabb verzió kell mindig a package.json-ben
-// depenciesben oda kell rakni a "^" karaktert a verzió elé
-var _ = require("lodash")
-var example = _.fill([1, 2, 3, 4, 5], "banana", 1, 4);
-console.log(example)
-// npm uninstall lodash
+app.get("/", (req, res) => {
+    res.end("Hello world!")
+})
+
+app.listen(8080)
