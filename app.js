fdf72e452f6dedd3627767fc14ab3e02b6cba0cf
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 16:33:07 +0200

    http-fs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var http = require("http")
-var server = http.createServer((req, res) => {
-    /*res.write("Hello world from nodejs")
-    res.end()*/
-    if (req.url === "/")
-        res.end("Hello world from nodejs")
-    else
-        res.end("Using some other domain")
-})
+var fs = require("fs")
+http.createServer((req, res) => {
+    if (req.url === "/html") {
+        var readStream = fs.createReadStream("./static/index.html")
+        res.writeHead(200, {"Content-type": "text/html"})
+        readStream.pipe(res)
+    } else if (req.url === "/image") {
+        var readStream = fs.createReadStream("./static/valami.jpg")
+        res.writeHead(200, {"Content-type": "image/jpg"})
+        readStream.pipe(res)
+    } else if (req.url === "/json") {
+        var readStream = fs.createReadStream("./static/valami.json")
+        res.writeHead(200, {"Content-type": "application/json"})
+        readStream.pipe(res)
+    } else
+        res.end("Nem ezt az oldalt keresed!")
 
-server.listen(8080)
+}).listen(8080)
