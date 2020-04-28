e6ee5838f2d80ff0054261854a4ae029ca5ab53c
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 16:45:57 +0200

    npm versions

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // npm install lodash
+
+// ha egy újabb verzió kell mindig a package.json-ben
+// depenciesben oda kell rakni a "^" karaktert a verzió elé
 var _ = require("lodash")
 var example = _.fill([1, 2, 3, 4, 5], "banana", 1, 4);
 console.log(example)
