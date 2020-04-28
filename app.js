7aa21162fef19e0d6abc973d4147517a76a12ba0
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 22:02:43 +0200

    JSON-Expresss-Body parser

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require("express")
 var path = require("path")
+var bodyParser = require("body-parser")
 var app = express()
 
 app.use("/public", express.static(path.join(__dirname, "static")))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "static", "index.html"))
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    // DB here
+    //res.send("Successfully posted data")
+    res.json({success : true})
 })
 
 app.get("/example", (req, res) => {
