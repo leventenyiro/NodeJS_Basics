ae262eeeb377fe9994b1e461d8fb4ed781da17df
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 19:35:58 +0200

    Serving static files with Express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require("express")
+var path = require("path")
 var app = express()
 
+app.use("/public", express.static(path.join(__dirname, "static")))
+
 app.get("/", (req, res) => {
-    res.end("Hello world!")
+    res.sendFile(path.join(__dirname, "static", "index.html"))
+})
+
+app.get("/example", (req, res) => {
+    res.send("Hitting example route")
+})
+
+app.get("/example/:name/:age", (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send(req.params.name + " : " + req.params.age)
 })
 
 app.listen(8080)
