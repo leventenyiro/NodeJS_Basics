d54ff8b5a11e234701c46da67c0eec5452174d42
leventenyiro <nyiro.levente@gmail.com>
2020-04-28 15:42:31 +0200

    ReadStream, WriteStream

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
 var fs = require("fs")
-fs.mkdir("tutorial", (err) => {
-    if (err) throw err
-    else {
-        fs.writeFile("./tutorial/valami.txt", "123", (err) => {
-            if (err) throw err
-            else console.log("Successful file create")
-        })
-    }
-})
-
-fs.unlink("./tutorial/valami.txt", (err) => {
-    if (err) throw err
-    else {
-        fs.rmdir("tutorial", (err) => {
-            if (err) throw err
-            else console.log("Successful folder delete")
-        })
-    }
-})
-
-fs.readdir("tutorial", (err, files) => {
-    if (err) throw err
-    else {
-        for (var file of files) {
-            fs.unlink("./tutorial/" + file, (err) => {
-                if (err) throw err
-                else console.log("Successful delete files")
-            })
-        }
-    }
+var readStream = fs.createReadStream("./valami.txt", "utf8")
+var writeStream = fs.createWriteStream("valami2.txt")
+readStream.on("data", (chunk) => {
+    writeStream.write(chunk) 
 })
