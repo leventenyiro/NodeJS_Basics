e5ab828e8af3220438e3aa4b747d210b11eba8a2
leventenyiro <nyiro.levente@gmail.com>
2020-04-29 15:32:49 +0200

    MiddleWare

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require("express")
-var path = require("path")
+var bodyparser = require("body-parser")
 var app = express()
 
-app.use("/public", express.static(path.join(__dirname, "static")))
-app.set("view engine", "ejs")
-
-app.get("/:userQuery", (req, res) => {
-    res.render("index",{data : {userQuery: req.params.userQuery,
-                        searchResults: ["book1", "book2", "book3"], 
-                        loggedIn: true, 
-                        username: "leventenyiro"}})
+app.use(bodyparser.json())
+app.use(/* "/example", */(req, res, next) => {
+    //console.log(req.url, req.method)
+    req.banana = "banana"
+    next()
 })
 
-app.listen(8080)
+app.get("/", (req, res) => {
+    console.log(req.banana)
+    res.send("Middleware")
+}).listen(8080)
